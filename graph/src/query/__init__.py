5ed688ca33454e7f2de62705dab469a2fc50f60f
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2026-02-19 22:15:08 -0800

    feat: change graph rag query import

--- graph/src/query/__init__.py
+++ graph/src/query/__init__.py
@@ -1,10 +1,8 @@
 from src.query.formatting import build_context, format_context_block
 from src.query.graph_rag_query import graph_rag_query
-from src.query.similar_bills_query import find_similar_bills
 
 __all__ = [
     "build_context",
     "format_context_block",
     "graph_rag_query",
-    "find_similar_bills",
 ]
